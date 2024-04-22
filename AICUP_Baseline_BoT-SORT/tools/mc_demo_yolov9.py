0d95f569499f275f6ad4c1a5e4f953369b2ba8f1
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-04-23 00:59:39 +1000

    Add files via upload

--- AICUP_Baseline_BoT-SORT/tools/mc_demo_yolov9.py
+++ AICUP_Baseline_BoT-SORT/tools/mc_demo_yolov9.py
@@ -13,16 +13,6 @@ from numpy import random
 sys.path.insert(0, './yolov9')
 sys.path.append('.')
 
-"""
-from yolov9.models.experimental import attempt_load
-from yolov9.utils.datasets import LoadStreams, LoadImages
-from yolov9.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, \
-    apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from yolov9.utils.plots import plot_one_box
-from yolov9.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
-"""
-
 from yolov7.utils.general import scale_coords
 from yolov7.utils.plots import plot_one_box
 

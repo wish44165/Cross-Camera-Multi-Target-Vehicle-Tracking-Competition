2c4ef5e06c59b872af066e670e9e355472010816
yuhsi chen <38905980+wish44165@users.noreply.github.com>
2024-04-22 17:20:51 +1000

    Add files via upload

---
+++ AICUP_Baseline_BoT-SORT/tools/mc_demo_yolov9.py
@@ -0,0 +1,273 @@
+import argparse
+import time
+from pathlib import Path
+import sys
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+from tqdm import tqdm
+from numpy import random
+
+sys.path.insert(0, './yolov9')
+sys.path.append('.')
+
+"""
+from yolov9.models.experimental import attempt_load
+from yolov9.utils.datasets import LoadStreams, LoadImages
+from yolov9.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, \
+    apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from yolov9.utils.plots import plot_one_box
+from yolov9.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+"""
+
+from yolov7.utils.general import scale_coords
+from yolov7.utils.plots import plot_one_box
+
+from yolov9.models.experimental import attempt_load
+from yolov9.models.common import DetectMultiBackend
+from yolov9.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from yolov9.utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
+                            apply_classifier, set_logging,    # added
+                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
+from yolov9.utils.plots import Annotator, colors, save_one_box
+from yolov9.utils.torch_utils import select_device, smart_inference_mode, time_sync
+
+from tracker.mc_bot_sort import BoTSORT
+from tracker.tracking_utils.timer import Timer
+
+
+def detect(save_img=False):
+    source, weights, view_img, save_txt, imgsz, trace = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size, opt.trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
+    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        ('rtsp://', 'rtmp://', 'http://', 'https://'))
+
+    # Directories
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+    # Initialize
+    set_logging()
+
+    # Load model
+    device = select_device(opt.device)
+    model = DetectMultiBackend(weights, device=device, dnn=opt.dnn, data=opt.data, fp16=opt.half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+
+    # Dataloader
+    bs = 1  # batch_size
+    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=1)
+    vid_path, vid_writer = [None] * bs, [None] * bs
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(100)]
+
+    # Create tracker
+    tracker = BoTSORT(opt, frame_rate=30.0)
+
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+        
+    t0 = time.time()
+    
+    # Process detections
+    results = []
+    frameID = 0
+
+
+
+    # Run inference
+    #model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    #for path, img, im0s, vid_cap, s in dataset:
+    for path, img, im0s, vid_cap, s in tqdm(dataset, desc=f'tracking {opt.name}'):
+        with dt[0]:
+            img = torch.from_numpy(img).to(model.device)
+            img = img.half() if model.fp16 else img.float()  # uint8 to fp16/32
+            img /= 255  # 0 - 255 to 0.0 - 1.0
+            if len(img.shape) == 3:
+                img = img[None]  # expand for batch dim
+
+        # Inference
+        t1 = time_sync()
+        with dt[1]:
+            #visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+            #pred = model(img, augment=augment, visualize=visualize)
+            pred = model(img, augment=False, visualize=False)
+            pred = pred[0][1]
+
+        # NMS
+        with dt[2]:
+            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=1000)
+        t2 = time_sync()
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, img, im0s)
+
+
+
+
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+            # Run tracker
+            detections = []
+            if len(det):
+                boxes = scale_coords(img.shape[2:], det[:, :4], im0.shape)
+                boxes = boxes.cpu().numpy()
+                detections = det.cpu().numpy()
+                detections[:, :4] = boxes
+
+            online_targets = tracker.update(detections, im0, frameID)
+
+            online_tlwhs = []
+            online_ids = []
+            online_scores = []
+            online_cls = []
+            for t in online_targets:
+                tlwh = t.tlwh
+                tlbr = t.tlbr
+                tid = t.track_id
+                tcls = t.cls
+                if tlwh[2] * tlwh[3] > opt.min_box_area:
+                    online_tlwhs.append(tlwh)
+                    online_ids.append(tid)
+                    online_scores.append(t.score)
+                    online_cls.append(t.cls)
+
+                    if save_img or view_img:  # Add bbox to image
+                        if opt.hide_labels_name:
+                            label = f'{tid}, {int(tcls)}'
+                        else:
+                            label = f'{tid}, {names[int(tcls)]}'
+                        
+                        if 'car' in label: # AICUP only have one cls: car
+                            # save results
+                            results.append(
+                                f"{frameID},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
+                            )
+
+                            plot_one_box(tlbr, im0, label=label, color=colors[int(tid) % len(colors)], line_thickness=2)
+
+                            
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+
+            # Print time (inference + NMS)
+            # print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+            # Stream results
+            if view_img:
+                cv2.imshow('BoT-SORT', im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+
+    if save_txt or save_img:
+        with open(save_dir / f"{opt.name}.txt", 'w') as f:
+            f.writelines(results)
+            
+        print(f"Results saved to {save_dir}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='/home/wish/pro/AICUP/MCMOT/AICUP_Baseline_BoT-SORT/yolov9/runs/train/yolov9-e/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=1920, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.09, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.7, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--trace', action='store_true', help='trace model')
+    parser.add_argument('--hide-labels-name', default=False, action='store_true', help='hide labels')
+
+    # v9
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--data', type=str, default='/home/wish/pro/AICUP/MCMOT/AICUP_Baseline_BoT-SORT/yolov9/data/AICUP.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+
+    # tracking args
+    parser.add_argument("--track_high_thresh", type=float, default=0.3, help="tracking confidence threshold")
+    parser.add_argument("--track_low_thresh", default=0.05, type=float, help="lowest detection threshold")
+    parser.add_argument("--new_track_thresh", default=0.4, type=float, help="new track thresh")
+    parser.add_argument("--track_buffer", type=int, default=30, help="the frames for keep lost tracks")
+    parser.add_argument("--match_thresh", type=float, default=0.7, help="matching threshold for tracking")
+    parser.add_argument("--aspect_ratio_thresh", type=float, default=1.6,
+                        help="threshold for filtering out boxes of which aspect ratio are above the given value.")
+    parser.add_argument('--min_box_area', type=float, default=10, help='filter out tiny boxes')
+    parser.add_argument("--fuse-score", dest="mot20", default=False, action="store_true",
+                        help="fuse score and iou for association")
+
+    # CMC
+    parser.add_argument("--cmc-method", default="sparseOptFlow", type=str, help="cmc method: sparseOptFlow | files (Vidstab GMC) | orb | ecc")
+
+    # ReID
+    parser.add_argument("--with-reid", dest="with_reid", default=False, action="store_true", help="with ReID module.")
+    parser.add_argument("--fast-reid-config", dest="fast_reid_config", default=r"fast_reid/configs/MOT17/sbs_S50.yml",
+                        type=str, help="reid config file path")
+    parser.add_argument("--fast-reid-weights", dest="fast_reid_weights", default=r"pretrained/mot17_sbs_S50.pth",
+                        type=str, help="reid config file path")
+    parser.add_argument('--proximity_thresh', type=float, default=0.5,
+                        help='threshold for rejecting low overlap reid matches')
+    parser.add_argument('--appearance_thresh', type=float, default=0.25,
+                        help='threshold for rejecting low appearance similarity reid matches')
+
+    opt = parser.parse_args()
+
+    opt.jde = False
+    opt.ablation = False
+
+    print(opt)
+    # check_requirements(exclude=('pycocotools', 'thop'))
+
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov7.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
